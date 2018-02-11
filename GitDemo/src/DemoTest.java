93c8e612faaa38ae16be30c79ebd7ea1c8b17b3d
Admin <Admin@192.168.1.3>
2018-02-11 09:41:11 +0530

    This is my forth commit to Master-11-2-2017-10:45AM
    Change-Id: Ic15c73a43e57c93051cc6ffc89c2039c2cb875fb
    Signed-off-by: Admin <Admin@192.168.1.3>

---
+++ GitDemo/src/DemoTest.java
@@ -0,0 +1,16 @@
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("First Commit");
+		
+		System.out.println("Second Commit");
+		
+		System.out.println("Fourth commmit to Master.");
+		
+
+	}
+
+}
